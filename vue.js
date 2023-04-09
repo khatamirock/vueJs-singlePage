bbd03262e46d2eae09ba4fe46100c48b1b29ba7d
ucheckit7@gmail.com <777788667555887777>
2023-04-09 23:32:43 +0600

    idk

--- vue.js
+++ vue.js
@@ -1,18 +1,17 @@
 
-// var app = {
-
-//     data() {
 
 
-//     }
-
-// };
-
-var navList = {
+var app = {
     data() {
 
         return {
             activePage: 0,
+            isDark: false,
+            theme: 'light',
+            note: '',
+            noteCol: 'lired',
+            clicked: [],
+            notes: [],
             page: [
                 { link: 'home.html', title: "Home", content: 'homer vaii' },
                 { link: 'bout.html', title: "About", content: "bout me!!!!" },
@@ -20,9 +19,51 @@ var navList = {
             msg: 'home apge'
 
         }
+    },
+    methods: {
+        kire: function () {
+            alert('vaii revii')
+        },
+        changeTheme: function () {
+            this.theme = this.theme == 'light' ? 'dark' : 'light'
+        },
+        notesave: function (msg) {
+
+            if (this.note != '') {
+                // alert(`Save Note: ${}`);
+                this.notes.push(this.note);
+                this.note = '';
+            }
+            else { alert('cant be empty'); }
+
+        },
+        makegreen: function (ind) {
+            if (!this.clicked.includes(ind)) { this.clicked.push(ind); }
+            else {
+                this.clicked.splice(ind - 1, 1);
+                // console.log('baddddd', ind);
+            }
+            console.log(this.clicked);
+            // this.noteCol = this.noteCol == 'lired' ? 'ligreen' : 'lired';
+        }
+
     }
+
+
+
 };
 
 
 // Vue.createApp(app).mount('#app');
-Vue.createApp(navList).mount('body');
+app = Vue.createApp(app)
+
+app.component('page-body',
+    {
+        props: ['page'],
+        template: `   <h1> {{page.title}} Page !!</h1>
+                     <p>{{page.content}}</p>`
+    }
+)
+
+
+app.mount('body');
